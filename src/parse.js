8113325ea6025ea310c9f24efc252c0da25179d0
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-07-02 23:42:08 +0900

    refactor: add remove blank function to parse.js
    - remove all blank to lib text

--- src/parse.js
+++ src/parse.js
@@ -3,10 +3,11 @@ import path from 'path'
 
 const hasElement = (arr) => !!arr.length;
 const getUrl = (text) => text.match(/(?<=\()([^\~]*)(?=\))/)[0]
+const removeBlank = (text) => text.replace(/\s+/g,"");
 const splitAndRemoveFirst = (text, splitLetter) => text.split(splitLetter).splice(1)
 
 const parseLib = (lib) => {
-  const [name, _, github, npm, docs] = splitAndRemoveFirst(lib, '|')
+  const [name, _, github, npm, docs] = splitAndRemoveFirst(removeBlank(lib), '|')
   return {
     name,
     github: getUrl(github),
