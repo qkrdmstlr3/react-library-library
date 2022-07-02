8e2d65d87bcd24eae0f0de60a17b949a87ce6f26
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-07-02 23:27:59 +0900

    feat: parse README.md and create json file
    - to create json file, implement parsing function

---
+++ src/parse.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path'
+
+const hasElement = (arr) => !!arr.length;
+const getUrl = (text) => text.match(/(?<=\()([^\~]*)(?=\))/)[0]
+const splitAndRemoveFirst = (text, splitLetter) => text.split(splitLetter).splice(1)
+
+const parseLib = (lib) => {
+  const [name, _, github, npm, docs] = splitAndRemoveFirst(lib, '|')
+  return {
+    name,
+    github: getUrl(github),
+    npm: npm ? getUrl(npm) : '',
+    docs: getUrl(docs),
+  }
+}
+
+const parseList = (json, category) => {
+  const [title, _, __, ___, ...libs] = category.split('\n');
+  json[title] = libs.filter(hasElement).map(parseLib);
+  return json;
+}
+
+(() => {
+  const readme = fs.readFileSync(path.join('./README.md'),  { encoding: 'utf-8' });
+  const libraryList = splitAndRemoveFirst(readme, '### ')
+  const reliliJSON = libraryList.reduce(parseList, {});
+  
+  const JSON_NAME = 'relili.json';
+  fs.writeFile(JSON_NAME, JSON.stringify(reliliJSON), () => {});
+})()
